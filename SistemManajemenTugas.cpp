c87c5e0cf5189b11b5102283a86aba36a17709c4
kezi <ezibelva08@gmail.com>
2026-04-26 02:47:32 +0700

    chore: inisialisasi file utama program

---
+++ SistemManajemenTugas.cpp
@@ -0,0 +1,18 @@
+// Nama [NIM] : Kanahaya Ezi     [124250046]
+//              Failasufa Elsya  [124250050]
+// Kelas      : SI - C
+// Tema       : Sistem Manajemen Tugas Kuliah
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << "Sistem Manajemen Tugas Kuliah" << endl;
+    cout << "Program berhasil dijalankan." << endl;
+
+    return 0;
+}
