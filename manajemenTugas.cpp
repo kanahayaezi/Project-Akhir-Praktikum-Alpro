b5991047816c6abaa7470d5e59c522c40fc1c7af
kezi <ezibelva08@gmail.com>
2026-05-10 02:27:07 +0700

    uptdate validasi deadline, input angka, dan imput konfirm

--- manajemenTugas.cpp
+++ manajemenTugas.cpp
@@ -48,6 +48,17 @@ int validDeadline(char dl[]) {
             return 0;
     }
 
+    int dd = (dl[0]-'0')*10 + (dl[1]-'0');
+    int mm = (dl[3]-'0')*10 + (dl[4]-'0');
+    int yy = (dl[6]-'0')*1000 + (dl[7]-'0')*100 + (dl[8]-'0')*10 + (dl[9]-'0');
+
+    if (dd < 1 || dd > 31) return 0;
+    if (mm < 1 || mm > 12) return 0;
+    if (yy < 2000 || yy > 2099) return 0;
+
+    int maxHari[13] = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+    if (dd > maxHari[mm]) return 0;
+
     return 1;
 }
 
@@ -56,7 +67,6 @@ int inputAngka(const char *label, int min, int max) {
 
     do {
         cout << label << ": ";
-        cin >> nilai;
 
         if (cin.fail()) {
             cin.clear();
@@ -64,12 +74,34 @@ int inputAngka(const char *label, int min, int max) {
 
             cout << "[!] Masukan harus berupa angka." << endl;
             nilai = min - 1;
+            continue;
+        }
+
+        cin.ignore();
+
+        if (nilai < min || nilai > max) {
+            cout << "[!] Masukkan angka antara " << min << " sampai " << max << ". Ulangi." << endl;
         }
 
     } while (nilai < min || nilai > max);
 
-    cin.ignore();
     return nilai;
+}
+
+char inputKonfirm(const char *pertanyaan) {
+    char jawab[10];
+
+    do {
+        cout << pertanyaan << " (y/t): ";
+        cin.getline(jawab, 10);
+
+        if (jawab[0] != 'y' && jawab[0] != 'Y' && jawab[0] != 't' && jawab[0] != 'T') {
+            cout << "[!] Masukkan 'y' untuk ya atau 't' untuk tidak. Ulangi." << endl;
+        }
+
+    } while (jawab[0] != 'y' && jawab[0] != 'Y' && jawab[0] != 't' && jawab[0] != 'T');
+
+    return jawab[0];
 }
 
 void inputTeks(const char *label, char hasil[], int batas) {
