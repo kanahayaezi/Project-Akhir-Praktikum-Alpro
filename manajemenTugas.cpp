f52236cae5fc5f5f1c3de773852c8fa3d44a156b
kezi <ezibelva08@gmail.com>
2026-05-09 23:09:09 +0700

    tambah validasi input

--- manajemenTugas.cpp
+++ manajemenTugas.cpp
@@ -26,6 +26,31 @@ int idCounter = 1;
 void bersihLayar() { system("cls"); }
 void jedaLayar() { system("pause"); }
 
+int kosongString(char str[]) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ') return 0;
+        i++;
+    }
+    return 1;
+}
+
+int validDeadline(char dl[]) {
+    int i;
+
+    if ((int)strlen(dl) != 10) return 0;
+    if (dl[2] != '/' || dl[5] != '/') return 0;
+
+    for (i = 0; i < 10; i++) {
+        if (i == 2 || i == 5) continue;
+
+        if (dl[i] < '0' || dl[i] > '9')
+            return 0;
+    }
+
+    return 1;
+}
+
 int main() {
     cout << "Program Manajemen Tugas Kuliah" << endl;
     return 0;
