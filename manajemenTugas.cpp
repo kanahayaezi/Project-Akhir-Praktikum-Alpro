5a1d41c1c901e9dc0ef29c4c9c175922f5240a26
kezi <ezibelva08@gmail.com>
2026-05-09 21:37:18 +0700

    tambah struct data tugas dan node linked list

--- manajemenTugas.cpp
+++ manajemenTugas.cpp
@@ -4,6 +4,25 @@
 #include <string.h>
 using namespace std;
 
+// struct data tugas
+struct DataTugas {
+    int id;
+    char matakuliah[30];
+    char judul[50];
+    char deadline[11];
+    int selesai;
+};
+
+// node linked list
+typedef struct Node *NodePtr;
+typedef struct Node {
+    DataTugas data;
+    NodePtr next;
+} Node;
+
+NodePtr head = NULL;
+int idCounter = 1;
+
 int main() {
     cout << "Program Manajemen Tugas Kuliah" << endl;
     return 0;
